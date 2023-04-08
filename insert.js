f91cf159aa925e29c484c49083811632edff80c2
Pietro Rocchio <pietro.rocchio@gpi.it>
2023-04-08 18:27:39 +0200

    Creata funziona di inserimento

--- insert.js
+++ insert.js
@@ -3,15 +3,52 @@
 const mysql = require("mysql2/promise");
 
 module.exports.insertTicket = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "La funzione insertTicket funziona!",
-        input: event,
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.PORT,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB_NAME,
+  });
+
+  try {
+
+    var data = JSON.parse(event.body.trim());
+    
+    
+    const [rows, fields] = await connection.execute(
+      "INSERT INTO Ticket (username, ip_caller, ip_server, url_path_server, patient, hospital, department, description, valid) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+      [
+        data.username,
+        data.ip_caller,
+        data.ip_server,
+        data.url_path_server,
+        data.patient,
+        data.hospital,
+        data.department,
+        data.description,
+        data.valid,
+      ]
+    );
+
+    connection.end();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-type": "application/json",
       },
-      null,
-      2
-    ),
-  };
+      body: JSON.stringify(rows),
+    };
+  } catch (error) {
+    connection.end();
+
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(error.message),
+    };
+  }
 };
