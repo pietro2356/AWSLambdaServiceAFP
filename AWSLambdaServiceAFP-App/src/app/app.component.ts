2f391836d768bbaf4af0fe47f28d2af0605383bd
Pietro Rocchio <pietro.rocchio@gpi.it>
2023-04-09 21:39:53 +0200

    Sviluppato codice di test per le prime funzionalita di ricezione dei Ticket

--- AWSLambdaServiceAFP-App/src/app/app.component.ts
+++ AWSLambdaServiceAFP-App/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AwsService } from './service/aws.service';
+import { Ticket } from './model/Ticket';
+import { tick } from '@angular/core/testing';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AWSLambdaServiceAFP-App';
+  ticket: Ticket[] = [];
+
+  constructor(private aws: AwsService){}
+
+  ngOnInit(): void {
+  }
+
+  execGet(){
+    const data = this.aws.getTicket().forEach(item => {
+      console.log(item);
+      console.table(item);
+
+      item.forEach((el: Ticket) => {
+        this.ticket.push(el);
+      })
+
+      console.log(this.ticket);
+
+    });
+
+
+
+    // TODO: Put data into ticket
+  }
 }
