3a6eb3183e56c2ced9aa5be10461408145f810bb
Pietro Rocchio <pietro.rocchio@gpi.it>
2023-04-13 00:39:37 +0200

    Aggiunti pulsanti di edit e modifica. Migliorata la funzionalità della tabella

--- AWSLambdaServiceAFP-App/src/app/components/tickettable/tickettable.component.ts
+++ AWSLambdaServiceAFP-App/src/app/components/tickettable/tickettable.component.ts
@@ -12,9 +12,10 @@ export class TickettableComponent {
   title = 'AWSLambdaServiceAFP-App';
   ticket: Ticket[] = [];
 
-  displayedColumns = ['id', 'username', 'patient', 'hospital', 'department', 'description'];
+  displayedColumns = ['id', 'username', 'patient', 'hospital', 'department', 'description', 'edit', 'delete'];
 
   dataSource: any;
+  selectedTicket?: Ticket;
 
   constructor(private aws: AwsService) {}
 
@@ -44,4 +45,13 @@ export class TickettableComponent {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+
+  editTicket(ticket: Ticket){
+    console.log("vuoi modificare il Ticket: ", ticket);
+  }
+
+  deleteTicket(ticket: Ticket){
+    console.log("vuoi eliminare il Ticket: ", ticket);
+  }
+
 }
