54a5fa89a0488e0472d0792931b16b3b88504752
Pietro Rocchio <pietro.rocchio@gpi.it>
2023-04-13 14:35:16 +0200

    Creata app Angular di base

--- Serverless/update.js
+++ Serverless/update.js
@@ -1,43 +1,44 @@
-"use strict";
+'use strict'
 
 const mysql = require("mysql2/promise");
 
 module.exports.updateTicket = async (event) => {
-  const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.PORT,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DB_NAME,
-  });
+    const connection = await mysql.createConnection({
+        host: "afphospitalmanagment.cqttky88vx96.us-east-1.rds.amazonaws.com",
+        port: 3306,
+        user: "admin",
+        password: "LguEz66SXdj3dSr",
+        database: "AFPHospitalTicketing"
+    });
 
-  try {
+    try {
 
-    let data = JSON.parse(event.body);
+        let data = JSON.parse(event.body.trim());
 
-    const [rows, fields] = await connection.execute(
-      "UPDATE Ticket SET description = ? WHERE id = ?",
-      [data.description, data.id]
-    );
+        const [rows, fields] = await connection.execute(
+            "UPDATE Ticket SET description = ? WHERE id = ?",
+            [data.description, data.id]
+        );
 
-    connection.end();
+        connection.end();
 
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(rows),
-    };
-  } catch (error) {
-    connection.end();
+        return {
+            statusCode: 200,
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(rows, null, 2)
+        };
 
-    return {
-      statusCode: 500,
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(error.message),
-    };
-  }
-};
+    } catch (error) {
+        connection.end();
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify(error.message),
+            headers: {
+                "Content-type": "application/json",
+            }
+        }
+    }
+}
